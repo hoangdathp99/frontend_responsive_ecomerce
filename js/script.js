22d8f208274964a61f8a7e44e090692c7fc1d420
DatHQ4 <hoangdat1999hp@gmail.com>
2022-03-02 15:13:24 +0700

    responsive header , navbar

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.nav-icon').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').addClass('active');
+        $('.cover').addClass('active');
+        $('.web-info').addClass('active-nav');
+    });
+    $('.cover').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').removeClass('active');
+        $('.cover').removeClass('active');
+        $('.web-info').removeClass('active-nav');
+    });
+    // $('nav-bar').click(function (e) { 
+    //     e.preventDefault();
+    //     $(this).find('.nav-item').addClass('active');
+    //     $(this).siblings().removeClass('active');
+    // });
+});
