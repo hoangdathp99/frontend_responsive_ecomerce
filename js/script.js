49e6adacd9cf0bc14a50ce732aeec9d5ab52d5aa
DatHQ4 <hoangdat1999hp@gmail.com>
2022-03-04 15:18:15 +0700

    add slider

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-    $('.nav-icon').click(function (e) { 
+$(document).ready(function() {
+    $('.nav-icon').click(function(e) {
         e.preventDefault();
         $('.navbar').addClass('active');
         $('.cover').addClass('active');
         $('.web-info').addClass('active-nav');
         $('.control-box').addClass('active-nav');
     });
-    $('.cover').click(function (e) { 
+    $('.cover').click(function(e) {
         e.preventDefault();
         $('.navbar').removeClass('active');
         $('.cover').removeClass('active');
@@ -18,4 +18,33 @@ $(document).ready(function () {
     //     $(this).find('.nav-item').addClass('active');
     //     $(this).siblings().removeClass('active');
     // });
+    $('.slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                dots: true
+            }
+
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
 });
