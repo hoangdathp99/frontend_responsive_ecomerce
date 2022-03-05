3042d7ae85dddb255b1225571af33533151d48b7
DatHQ4 <hoangdat1999hp@gmail.com>
2022-03-06 02:48:09 +0700

    design footer, perfect pixel, responsive

--- js/script.js
+++ js/script.js
@@ -13,6 +13,54 @@ $(document).ready(function() {
         $('.web-info').removeClass('active-nav');
         $('.control-box').removeClass('active-nav');
     });
+    $(".show").click(function(e) {
+        e.preventDefault();
+        $('.about-content').toggle('show');
+        $('.show').toggle('hide');
+        $('.hide').toggle('show');
+        $('.social').toggle('hide');
+
+    });
+    $(".hide").click(function(e) {
+        e.preventDefault();
+        $('.about-content').toggle('hide');
+        $('.show').toggle('show');
+        $('.hide').toggle('hide');
+        $('.social').toggle('show');
+    });
+    $(".show-left").click(function(e) {
+        e.preventDefault();
+        $('.nav-item-right').toggle('show');
+        // $('.show').toggle('hide');
+        // $('.hide').toggle('show');
+        // $('.social').toggle('hide');
+
+    });
+    $(".show-right").click(function(e) {
+        e.preventDefault();
+        $('.nav-item').toggle('show');
+
+    });
+    $(".address-mobile").click(function(e) {
+        e.preventDefault();
+        $(this).find('.content').toggle('show');
+        $('.phone-mobile').toggle('hide');
+        $('.mail-mobile').toggle('hide');
+    });
+    $(".phone-mobile").click(function(e) {
+        e.preventDefault();
+        $(this).find('.content').toggle('show');
+
+        $('.address-mobile').toggle('hide');
+        $('.mail-mobile').toggle('hide');
+    });
+    $(".mail-mobile").click(function(e) {
+        e.preventDefault();
+        $(this).find('.content').toggle('show');
+
+        $('.phone-mobile').toggle('hide');
+        $('.address-mobile').toggle('hide');
+    });
     // $('nav-bar').click(function (e) { 
     //     e.preventDefault();
     //     $(this).find('.nav-item').addClass('active');
